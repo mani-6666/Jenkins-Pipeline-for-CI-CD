b9bf45ca5b1d0060a02fc0d9c4f091d5be7b121d
Manikanta <107737557+mani-6666@users.noreply.github.com>
2025-05-13 16:01:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello, Elevate Labs, This is before pusing'));
+app.get('/', (req, res) => res.send('Hello, Elevate Labs, This is After pushing code commits to GitHub'));
 app.listen(3000, () => console.log('App running on port 3000'));
